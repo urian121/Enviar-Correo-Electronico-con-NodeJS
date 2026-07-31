e213948c8dc55dea1386d5168ddfcf220b076aa6
URIAN VIERA <urian1213viera@gmail.com>
2026-07-31 14:09:49 -0500

    Update enviarCorreo.js

--- enviarCorreo.js
+++ enviarCorreo.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "urianwebdeveloper@gmail.com",
-    pass: "tcgsaaiuilyreuzc",
+    pass: "xxxxxxxxxxx",
   },
 });
 
